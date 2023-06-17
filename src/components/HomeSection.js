48e5bd9ee217bbb21c19d0870631268f04ee060a
Jun Lee <jun@Juns-MBP-2.lan>
2023-06-17 17:35:17 -0500

    ref feature added

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,5 +1,6 @@
 import "./HomeSection.css";
 import { SocialIcon } from "react-social-icons";
+import { useRef } from "react";
 import jun from "../assets/JunLee.webp";
 import html from "../assets/html-5.webp";
 import css from "../assets/css-3.webp";
@@ -10,6 +11,15 @@ import nextJS from "../assets/nextjs.webp";
 import nodeJS from "../assets/nodejs.webp";
 
 function HomeSection() {
+    const homeRef = useRef(null);
+    const portfolioRef = useRef(null);
+    const clickToHome = () => {
+        homeRef.current?.scrollIntoView({behavior: "smooth"});
+    }
+    const clickToPortfolio = () => {
+        portfolioRef.current?.scrollIntoView({behavior: "smooth"});
+    }
+
   return (
     <>
       <div className="navbar-container">
@@ -30,25 +40,28 @@ function HomeSection() {
                 url="https://www.linkedin.com/in/jeong-hyun-lee-a5362319a/"
                 bgColor="white"
                 fgColor="black"
+                className="icon"
                 style={{ marginRight: "15px", width: "35px", height: "35px" }}
               />
               <SocialIcon
                 url="https://github.com/JunLee8108"
                 bgColor="white"
                 fgColor="black"
+                className="icon"
                 style={{ marginRight: "15px", width: "35px", height: "35px" }}
               />
               <SocialIcon
                 url="https://velog.io/@lejhn1"
                 bgColor="white"
                 fgColor="black"
+                className="icon"
                 style={{ width: "35px", height: "35px" }}
               />
             </div>
 
             <div className="navbar-item">
-              <li>About Me</li>
-              <li>Portfolio</li>
+              <li onClick={clickToHome}>About Me</li>
+              <li onClick={clickToPortfolio}>Portfolio</li>
               <li>Resume</li>
               <li>Contact Me</li>
             </div>
@@ -56,7 +69,7 @@ function HomeSection() {
         </ul>
       </div>
 
-      <div className="home-top-1 container-layout">
+      <div className="home-top-1 container-layout" ref={homeRef}>
         <div className="top-1-flexbox1">
           <h1 className="h1-green-left">Jeong Hyun Lee</h1>
           <h4 style={{ color: "grey" }}>Frontend Developer</h4>
@@ -66,7 +79,7 @@ function HomeSection() {
             efficient code and specialized in frontend development for complex
             web apps.
           </p>
-          <button>View Portfolio</button>
+          <button onClick={clickToPortfolio}>View Portfolio</button>
         </div>
         <div className="top-1-flexbox2"></div>
       </div>
@@ -79,42 +92,42 @@ function HomeSection() {
               <img src={html}></img>
               <img src={css}></img>
             </div>
-            <p>HTML & CSS</p>
+            <h4>HTML & CSS</h4>
           </div>
           <div className="top-2-flexbox">
             <div style={{ width: "100%" }}>
               <img src={js}></img>
             </div>
-            <p>Vanilla Javascript</p>
+            <h4>Vanilla Javascript</h4>
           </div>
           <div className="top-2-flexbox">
             <div style={{ width: "100%" }}>
               <img src={react}></img>
             </div>
-            <p>React</p>
+            <h4>React</h4>
           </div>
           <div className="top-2-flexbox">
             <div style={{ width: "100%" }}>
               <img src={vue}></img>
             </div>
-            <p>Vue</p>
+            <h4>Vue</h4>
           </div>
           <div className="top-2-flexbox">
             <div style={{ width: "100%" }}>
               <img src={nextJS}></img>
             </div>
-            <p>Next.js</p>
+            <h4>Next.js</h4>
           </div>
           <div className="top-2-flexbox">
             <div style={{ width: "100%" }}>
               <img src={nodeJS}></img>
             </div>
-            <p>Node.js</p>
+            <h4>Node.js</h4>
           </div>
         </div>
       </div>
 
-      <div className="home-mid container-layout">
+      <div className="home-mid container-layout" ref={portfolioRef}>
         <h1 className="h1-green-left">Portfolio</h1>
         <h3>1. Hyundai Motors</h3>
       </div>
