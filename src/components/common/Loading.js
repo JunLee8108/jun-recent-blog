1d6fd8f04cae59ae756d8f19fdcace927a20844f
JunLee8108 <lejhn1@gmail.com>
2024-03-05 21:49:02 -0600

    no animation effect

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -7,7 +7,7 @@ export default function Loading() {
     <>
       <div className="loading-container-bg">
         <div className="loading-container">
-          <HashLoader color="#39ff14" />
+          <HashLoader color="green" />
         </div>
       </div>
     </>
